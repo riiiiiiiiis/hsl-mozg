46f2a6ac4ec5b1b316e0be17fffe002ac6709fa8
riiiiiiiis <riiiiiiis@icloud.com>
2025-08-13 12:53:25 -0300

    fix: добавлена функция escape_markdown_v2 в utils/__init__.py
    Исправлена ошибка ImportError при деплое

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,6 @@
 # utils package
+
+def escape_markdown_v2(text: str) -> str:
+    """Helper function to escape text for MarkdownV2 parsing."""
+    escape_chars = r'_*[]()~`>#+-=|{}.!'
+    return "".join(f'\\{char}' if char in escape_chars else char for char in str(text))
